9b6ecb4cf620188fc102036823e96466a557631f
Vladyslav Domariev <vlad.domariev@gmail.com>
2025-01-13 16:34:23 +0100

    Getting maxId of folder

--- 1_step_create_packages.sh
+++ 1_step_create_packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Example command: sh 1_step_create_packages.sh -i 1_step_create_packages.sh -o OUTPUT
+# cksum - check sum. First parameter hash-code. Second file size. THird parameter: filename.
 
 while getopts i:o: flag
 do
@@ -9,15 +10,21 @@ do
         # f) fullname=${OPTARG};;
     esac
 done
-echo "Username: $inputpath";
-echo "Age: $outputpath";
-# echo "Full Name: $fullname";
-
-echo "hello world!";
-cat $inputpath;
-# cksum - check sum. First parameter hash-code. Second file size. THird parameter: filename.
+# echo "Username: $inputpath";
+# echo "Age: $outputpath";
+pwd;
 
 
-
+ls -vrd "$inputpath";
 
 # 1. Group files.
+
+# -v -> sort by number. -r -> reverse (DESC)
+# ls -vr "$inputpath" | grep -x -E '[[:digit:]]+'/ | head -1; # Version 1
+
+
+max_folder_id=$(ls -vr $inputpath | grep -x -E '[[:digit:]]+' | head -1);
+
+# max_folder_id=${ls -vr $inputpath | grep -x -E '[[:digit:]]+'/ | head -1};
+echo "max_folder_id: $max_folder_id";
+
